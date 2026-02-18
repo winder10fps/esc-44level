af37643dbf65840c70446fd0887652aa6e2c8725
winder10fps <samsungplanshetdns19@gmail.com>
2026-02-18 21:53:05 +0300

    изменил цветовую палитру

--- constants/ui.ts
+++ constants/ui.ts
@@ -1,12 +1,22 @@
 export const COLORS = {
-    BACKGROUND: "#0D0D0D",
-    SECTION_BACKGROUND: "#1A1A1A",
-    CARD_BACKGROUND: "#262626",
+    BACKGROUND: "#000000",
+    SECTION_BACKGROUND: "#141414",
+    CARD_BACKGROUND: "#212121",
     PRIMARY: "#FF3B30",
-    WHITE: "#FFFFFF",
-    GRAY: "#D3D3D3",
+    WHITE: "#f6f6f6",
+    GRAY: "#bababa",
     SEPARATOR: "#474747"
 }
+
+// export const COLORS = {
+//     BACKGROUND: "#0D0D0D",
+//     SECTION_BACKGROUND: "#1A1A1A",
+//     CARD_BACKGROUND: "#262626",
+//     PRIMARY: "#FF3B30",
+//     WHITE: "#FFFFFF",
+//     GRAY: "#D3D3D3",
+//     SEPARATOR: "#474747"
+// }
 
 export const FONT_FAMILIES = {
     ROBOTO: "Roboto",
